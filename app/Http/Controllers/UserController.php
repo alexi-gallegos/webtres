7bc78c0e258a0d926b3360f2a4782c4cf340be26
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 11:21:09 -0400

    agregar método para retornar vista y datos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+    public function index(){
+        $users = new User;
+        $users = $users->get_users();
+        
+        return view('index', [ 'users' => $users ]);
+    }
+
 }
