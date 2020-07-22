797bc67bb52399ff55833d983b4cc22aea3ad6f2
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 18:07:56 -0400

    importar jquery

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -13,6 +13,7 @@
     <div class="container-fluid">
         @yield('content')
     </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
 </body>
