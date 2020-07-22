91e24b3b68d695d6a700cd6d71847fd566dec708
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 18:06:33 -0400

    agregar ruta para consultar datos y crear usuario

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users', 'UserAPIController@get_users');
+Route::post('users', 'UserAPIController@store');
