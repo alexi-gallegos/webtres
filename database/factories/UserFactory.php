6c238964d0a14698c3c27aa183d13cb6227989d8
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 10:17:17 -0400

    crear factory de User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,12 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'nombres' => $faker->name,
+        'apellido_paterno' => $faker->lastName,
+        'apellido_materno' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'fecha_nacimiento' => $faker->date,
+        'password' => Hash::make('password'), // password
+        'path_to_image' => 'storage/images/user.webp'
     ];
 });
