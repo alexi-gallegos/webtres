a32c4cc79ab546e91dbbb29527ec8db9c11898b3
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 10:15:31 -0400

    agregar campo SoftDelete a migración User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('imagen');
             $table->string('email')->unique();
             $table->string('password');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
