74dacfed1b293ad08bcfa20dcfdd162ef9e4e90c
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-22 12:56:09 -0400

    agregar header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+@push('styles')
+    <style>
+        .jumbotron{
+            padding: 1rem;
+        }
+    </style>
+@endpush
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+      <h1 class="display-4">Administrador de usuarios</h1>
+    </div>
+</div>
