3d59e73d5d56fabea6dbf1da1bb7cb37e9f5a324
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-23 15:34:41 -0400

    agregar botón para volver a página usuarios

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 @push('styles')
     <style>
         .jumbotron{
-            padding: 1rem;
+            padding: .7rem;
         }
     </style>
 @endpush
@@ -10,4 +10,9 @@
     <div class="container text-center">
       <h1 class="display-4">Administrador de usuarios</h1>
     </div>
+    
+    @isset($show_inicio)
+        <a class="btn btn-primary" href="{{ route('users_inicio') }}">Ir a Usuarios</a>
+    @endisset
+
 </div>
