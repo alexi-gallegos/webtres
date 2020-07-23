dd12600579ea357e9a70055b4f07d66ab1361cfb
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-23 13:56:34 -0400

    agregar metodo 'store' en Modelo User

--- app/User.php
+++ app/User.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Image;
+use Illuminate\Support\Facades\Storage;
+
 \Carbon\Carbon::setToStringFormat('d/m/Y');
 
 
@@ -47,6 +50,26 @@ class User extends Authenticatable
         return $this::select('rut', 'nombres', 'apellido_paterno', 'apellido_materno','fecha_nacimiento', 'email', 'imagen')
                                     ->orderBy('created_at', 'desc')
                                     ->paginate(6);
+    }
+
+    public function store($request){
+        if($request->hasFile('imagen')){
+            $file = $request->file('imagen');
+            $img = Image::make($file->getRealPath());
+            $img->resize(200, 200, function ($constraint) {
+                $constraint->aspectRatio();
+            })->stream();
+            $filename = uniqid().'.'.$file->getClientOriginalExtension();
+            Storage::disk('images')->put($filename,   $img);
+        }
+        $this->nombres = $request->nombres;
+        $this->apellido_paterno = $request->apellido_paterno;
+        $this->apellido_materno = $request->apellido_materno;
+        $this->rut = $request->rut;
+        $this->fecha_nacimiento = $request->fecha_nacimiento;
+        $this->imagen = $filename;
+        $this->email = $request->email;
+        $this->password = $request->password;
     }
 
 }
