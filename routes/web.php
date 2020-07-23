e3830b19b845b7e7c4c3f973b3bea14264331d9e
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-23 15:33:51 -0400

    agregar nombre a rutas

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/users');
-Route::get('users', 'UserController@index');
-Route::get('users/create', 'UserController@create');
+Route::get('users', 'UserController@index')->name('users_inicio');
+Route::get('users/create', 'UserController@create')->name('users_create');
 
