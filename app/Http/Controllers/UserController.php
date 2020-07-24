f2dac7043e0131e4aabd776f5fce57f60288a339
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-24 15:35:30 -0400

    agregar funciones para eliminar y editar usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,6 +28,18 @@ class UserController extends Controller
     }
 
     public function update(EditUserRequest $request){
-        return $request;
+        $user = new User;
+        $user_to_update = $user->get_user($request->id);
+        $user_to_update->update_user($request);
+        $user_to_update->save();
+
+        return redirect()->route('users_inicio')->with('success', 'Usuario actualizado.');   
+    }
+
+    public function delete(Request $request){
+        $user = new User;
+        $usuario_to_delete = $user->get_user($request->id);
+        $usuario_to_delete->delete();
+        return redirect()->route('users_inicio')->with('success', 'Usuario eliminado.');
     }
 }
