5f99bdbc190cc92b516d3332eedaf8cb0c38ff73
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-24 10:02:57 -0400

    agregar métodos para manejar edición de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\EditUserRequest;
 
 class UserController extends Controller
 {
@@ -19,4 +20,14 @@ class UserController extends Controller
         return view('users.create');
     }
 
+    public function edit(Request $request){
+        $user = new User;
+        $usuario = $user->get_user($request->id);
+        return view('users.edit', [ 'user' => $usuario ]);
+
+    }
+
+    public function update(EditUserRequest $request){
+        return $request;
+    }
 }
