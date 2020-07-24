0787f6712d536a85549bb8dcb859729b1468fcda
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-24 09:59:15 -0400

    agregar rutas para editar usuario

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/users');
 Route::get('users', 'UserController@index')->name('users_inicio');
 Route::get('users/create', 'UserController@create')->name('users_create');
+Route::get('users/edit/{id}', 'UserController@edit')->name('users_edit');
+Route::patch('users', 'UserController@update')->name('users_update');
 
