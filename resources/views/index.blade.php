aecb0f01fc0fb7622bbc39c69c3d2311915612f0
Alexi Gallegos <alexi.gallegosp@gmail.com>
2020-07-24 10:04:56 -0400

    agregar botón para nuevo usuario y cambiar estilos tabla

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,21 @@
 
 @push('styles')
     <style>
-        table{
+        td, th{
             white-space: nowrap;
+        }
+        .table{
+          font-size: .85rem;
         }
     </style>
 @endpush
 
 @section('content')
 @include('partials.header')
+<div class="row col-md-11 mx-auto">
+  <h3>Usuarios</h3>
+  <a class="btn btn-primary ml-auto" href="{{ route('users_create') }}">Nuevo</a>
+</div>
 <table class="table col-md-9 mx-auto table-striped">
     <thead>
       <tr>
@@ -28,22 +35,34 @@
     <tbody>
       @foreach ($users as $user)
         <tr>
-            <th scope="row">1</th>
-            <td>{{ $user->nombres }}</td>
-            <td>{{ $user->apellido_paterno }}</td>
-            <td>{{ $user->apellido_materno }}</td>
+            <th scope="row">
+              <img 
+                  class="rounded-circle"
+                  src="{{ asset("storage/images/$user->imagen") }}" 
+                  width="40px" 
+                  alt="{{ "Imagen de $user->nombres $user->apellido_paterno" }}">
+            </th>
+            <td class="text-capitalize">{{ $user->nombres }}</td>
+            <td class="text-capitalize">{{ $user->apellido_paterno }}</td>
+            <td class="text-capitalize">{{ $user->apellido_materno }}</td>
             <td>{{ $user->rut }}</td>
-            <td>{{ $user->fecha_nacimiento }}</td>
+            <td>{{ \Carbon\Carbon::parse($user->fecha_nacimiento)->format('d/m/Y') }}</td>
             <td>{{ $user->email }}</td>
             <td>
-                <button class="btn btn-sm btn-danger">eliminar</button>
-                <button class="btn btn-sm btn-warning ml-2">editar</button>
+                <a class="btn btn-sm btn-danger" href="">
+                    <i class="fas fa-trash"></i>
+                </a>
+                <a 
+                  class="btn btn-sm btn-warning" 
+                  href="{{ route('users_edit', ['id' => $user->id ]) }}"
+                >
+                  <i class="fas fa-edit"></i>
+                </a>
             </td>
         </tr>
       @endforeach
     </tbody>
   </table>
-
     <div class="row justify-content-center">
         {{ $users->links() }}
     </div>
